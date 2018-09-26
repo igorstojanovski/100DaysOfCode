3b64c1c74984c18dc6f2c20b554162fb7f3a366c
Igor Stojanovski <igorce@outlook.com>
2018-09-26 23:25:10 +0200

    Add new Results table

--- src/main/java/co/igorski/hundreddays/web/ResultsView.java
+++ src/main/java/co/igorski/hundreddays/web/ResultsView.java
@@ -8,6 +8,8 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.Route;
@@ -16,14 +18,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 @Route("results")
 @Push(transport = Transport.LONG_POLLING)
-public class ResultsView extends VerticalLayout implements HasUrlParameter<String> {
+public class ResultsView extends VerticalLayout implements HasUrlParameter<String>, AfterNavigationObserver {
 
+    private final RunRepository runRepository;
+    private final RunStore runStore;
     private String runId;
+    private final Grid<Result> grid;
 
-    public ResultsView(@Autowired RunRepository runRepository, @Autowired RunStore runStore) {
+    @Autowired
+    public ResultsView(RunRepository runRepository, RunStore runStore) {
+        this.runRepository = runRepository;
+        this.runStore = runStore;
+        grid = new Grid<>();
+        add(grid);
+    }
 
-        Grid<Result> grid = new Grid<>();
+    @Override
+    public void setParameter(BeforeEvent event, String parameter) {
+        runId = parameter;
+    }
 
+    @Override
+    public void afterNavigation(AfterNavigationEvent event) {
         if(!runStore.containsId(runId)) {
             Run run = runRepository.findById(runId).get();
             grid.setDataProvider(new ListDataProvider<>(run.getResults()));
@@ -32,12 +48,5 @@ public class ResultsView extends VerticalLayout implements HasUrlParameter<Strin
         grid.addColumn(item -> item.getTest().getTestName()).setHeader("Test Name");
         grid.addColumn(Result::getStatus).setHeader("Status");
         grid.addColumn(Result::getOutcome).setHeader("Outcome");
-
-        add(grid);
-    }
-
-    @Override
-    public void setParameter(BeforeEvent event, String parameter) {
-        runId = parameter;
     }
 }
