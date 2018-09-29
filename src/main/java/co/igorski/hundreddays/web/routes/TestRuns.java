03eff9ef7ad31b9fd49ad372ef1a5b4b365b1587
Igor Stojanovski <igorce@outlook.com>
2018-09-29 11:37:46 +0200

    Autowire the PageableRunProvider

--- src/main/java/co/igorski/hundreddays/web/routes/TestRuns.java
+++ src/main/java/co/igorski/hundreddays/web/routes/TestRuns.java
@@ -1,15 +1,13 @@
 package co.igorski.hundreddays.web.routes;
 
 import co.igorski.hundreddays.model.Run;
-import co.igorski.hundreddays.repositories.RunRepository;
-import co.igorski.hundreddays.services.RunService;
-import co.igorski.hundreddays.web.dataproviders.PageableRunProvider;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.renderer.LocalDateTimeRenderer;
 import com.vaadin.flow.function.ValueProvider;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.artur.spring.dataprovider.PageableDataProvider;
 
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -18,7 +16,7 @@ import java.time.format.FormatStyle;
 @Route("runs")
 public class TestRuns extends VerticalLayout {
 
-    public TestRuns(@Autowired RunService runService, @Autowired RunRepository runRepository) {
+    public TestRuns(@Autowired PageableDataProvider<Run, Void> pageableDataProvider) {
         Grid<Run> grid = new Grid<>();
         grid.addColumn(Run::getId).setHeader("ID");
         grid.addColumn((ValueProvider<Run, Integer>) run -> run.getResults().size()).setHeader("CcTest Count");
@@ -29,7 +27,7 @@ public class TestRuns extends VerticalLayout {
                 )
         ).setHeader("Start");
 
-        grid.setDataProvider(new PageableRunProvider<Run, Void>());
+        grid.setDataProvider(pageableDataProvider);
 
         add(grid);
     }
