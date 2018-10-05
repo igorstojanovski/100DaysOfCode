d9694943d60838c6921471457cc55e3b6441a5e1
Igor Stojanovski <igorce@outlook.com>
2018-10-05 22:32:54 +0200

    Add unfinished runs handler
    The finish run event might not arrive for
    a reason. In that case the run should be
    automatically closed after certain amount
    of time.

--- src/main/java/co/igorski/hundreddays/stores/RunStore.java
+++ src/main/java/co/igorski/hundreddays/stores/RunStore.java
@@ -1,13 +1,21 @@
 package co.igorski.hundreddays.stores;
 
 import co.igorski.hundreddays.model.Run;
+import co.igorski.hundreddays.model.events.Event;
+import co.igorski.hundreddays.model.events.RunFinished;
 import co.igorski.hundreddays.web.DataListener;
 import com.vaadin.flow.spring.annotation.SpringComponent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +23,8 @@ import java.util.Map;
 @SpringComponent
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class RunStore {
+    @Autowired
+    private KafkaTemplate<String, Event> template;
     private final Map<String, Run> activeRuns = new HashMap<>();
     private final List<DataListener> listeners = new ArrayList<>();
 
@@ -61,5 +71,19 @@ public class RunStore {
         }
 
         return runExists;
+    }
+
+    @Scheduled(fixedRate = 10000)
+    public void reportCurrentTime() {
+        for(Run run : activeRuns.values()) {
+            LocalDateTime runLocalDateTime = run.getStart().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            if(runLocalDateTime.isBefore(LocalDateTime.now().minusMinutes(3))) {
+                RunFinished testFinished = new RunFinished();
+                testFinished.setRunId(run.getId());
+                testFinished.setTimestamp(new Date());
+
+                template.send("test-events", testFinished);
+            }
+        }
     }
 }
