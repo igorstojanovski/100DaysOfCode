9932f032e008bf41f270e2a9d43c9615605bc95d
Igor Stojanovski <igorce@outlook.com>
2018-12-02 23:39:25 +0100

    fix(Server): Align server with client

--- server/src/main/java/co/igorski/centralcommittee/model/Run.java
+++ server/src/main/java/co/igorski/centralcommittee/model/Run.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class Run {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "run_id")
     private Long id;
     @ManyToOne(cascade = CascadeType.ALL)
     private User user;
@@ -23,7 +24,7 @@ public class Run {
     private Date startTime;
     private Date endTime;
     //    @ElementCollection(targetClass = Entry.class, fetch = FetchType.EAGER)
-    @OneToMany(mappedBy = "id", cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Entry> entries;
 
 }
