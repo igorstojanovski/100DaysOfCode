6c20ffcde882b0337ce01177b584a922e6d71b79
Igor Stojanovski <igorce@outlook.com>
2019-01-27 15:20:03 +0100

    chore: Optimize imports

--- junit-5-extension/src/main/java/co/igorski/model/TestModel.java
+++ junit-5-extension/src/main/java/co/igorski/model/TestModel.java
@@ -4,8 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 @Getter
--- junit-5-extension/src/test/java/co/igorski/client/ApacheHttpClientTest.java
+++
@@ -1,6 +0,0 @@
-package co.igorski.client;
-
-class ApacheHttpClientTest {
-
-
-}
